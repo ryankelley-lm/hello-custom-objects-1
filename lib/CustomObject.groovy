28c69f5bb56937aafa650cb87421ec45edf355de
Edmund Miller <edmund.a.miller@protonmail.com>
2022-05-05 18:46:50 -0500

    fix: Use only native types

--- lib/CustomObject.groovy
+++ lib/CustomObject.groovy
@@ -3,16 +3,13 @@ import nextflow.io.ValueObject
 import nextflow.util.KryoHelper
 
 @ValueObject
-@Immutable(copyWith=true, knownImmutables = ['name', 'r1', 'r2'])
+@Immutable(copyWith=true, knownImmutables = ['id', 'single_end'])
 class CustomObject {
     static {
         // Register this class with the Kryo framework that serializes and deserializes objects
         // that pass through channles. This allows for caching when this object is used.
         KryoHelper.register(CustomObject)
     }
-    String name
-    /** The path to the read one FASTQ */
-    Path r1
-    /** The path to the read two FASTQ */
-    Path r2
+    String id
+    Boolean single_end
 }
